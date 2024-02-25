d6cde6e7acd5c26e50b34218d46e11c8e22c274b
HectorRuiz117 <126829320+HectorRuiz117@users.noreply.github.com>
2024-02-24 18:39:05 -0600

    added menu display and some PB progress
    This commit allows the user to switch between menus and implements some flawed PB progress. 
    Things that need to be changed:
    *PB bar increases horizontally
    *PB bar pushes icon to corners sometimes
    *PB progress does not increase in proportion to flag
    *PB bar does not have text

--- script.js
+++ script.js
@@ -1,51 +1,62 @@
 //Injection
 const toolbarHTML = `
-<div class="toolbar" id="toolbar">
-<button id="fontAlterationButton">
+<div class="toolbar" id="toolbarMain">
+<button class="mainButtons" id="fontAlterationButton">
     <img src="${chrome.runtime.getURL("images/fontAlteration.svg")}">
 </button>
-<button id="taskListButton">
+<button class="mainButtons" id="taskListButton">
     <img src="${chrome.runtime.getURL("images/taskList.svg")}">
 </button>
-<button id="progressBarButton">
+<button class="mainButtons" id="PBButton">
     <img src="${chrome.runtime.getURL("images/progressBar.svg")}">
 </button>
-<button id="timersButton">
+<button class="mainButtons" id="timersButton">
     <img src="${chrome.runtime.getURL("images/timers.svg")}">
 </button>
-<button id="fidgetButton">
+<button class="mainButtons" id="fidgetButton">
     <img src="${chrome.runtime.getURL("images/fidget.svg")}">
 </button>
-<button id="AGEButton">
+<button class="mainButtons" id="AGEButton">
     <img src="${chrome.runtime.getURL("images/AGE.svg")}">
 </button>
-<button id="settingsButton">
+<button class="mainButtons" id="settingsButton">
     <img src="${chrome.runtime.getURL("images/settings.svg")}">
+</button>
+<button class="backButtons fontAlterationButtons" id="backButtonFA">
+    <img src="${chrome.runtime.getURL("images/back.svg")}">
+</button>
+<button class="fontAlterationButtons" id="fontConversionButton">
+    <img src="${chrome.runtime.getURL("images/convert.svg")}">
+</button>
+<button class="fontAlterationButtons" id="bionicButton">
+    <img src="${chrome.runtime.getURL("images/bionic.svg")}">
+</button>
+<button class="fontAlterationButtons" id="toDefaultButton">
+    <img src="${chrome.runtime.getURL("images/defaultConvert.svg")}">
+</button>
+<button class="backButtons PBButtons" id="backButtonPB">
+    <img src="${chrome.runtime.getURL("images/back.svg")}">
+</button>
+<button class = "PBButtons" id = "PBDragFlag">
+    <img id = "flag" src="${chrome.runtime.getURL("images/progressBar.svg")}" draggable = "true">
+</button>
+
+<div class = "PBLive" id = "PBLive">
+    <div class = "progressBar">
+        <div class = "progress" id = "PBLiveProgress"></dive>
+        <div class = "progressText" id = "PBLiveProgressText"></dive>
+    </div>
+</div>
+<button class = "PBButtons" id = "PBDelete">
+    <img src="${chrome.runtime.getURL("images/trashIcon.svg")}">
 </button>
 </div>
 <div class="menu" id="fontAlterationMenu">
-<!-- Menu content here -->
 <ul>
     <li>Option 1</li>
     <li>Option 2</li>
     <li>Option 3</li>
 </ul>
-</div>
-
-<div class = "toolbar" id = "progressMenu">
-<button id="backButton">
-    <img src="${chrome.runtime.getURL("images/back.svg")}">
-</button>
-<button id="dragFlag">
-    <img id = "flag" src="${chrome.runtime.getURL("images/progressBar.svg")}" draggable="true">
-</button>
-
-<!-- PB BAR -->
-<div class = "progressBarContainer" id = "PBC"></div>  
-
-<button id = "trashIcon">
-    <img src="${chrome.runtime.getURL("images/trashIcon.svg")}">
-</button>
 </div>
 `;
 document.body.insertAdjacentHTML('afterbegin', toolbarHTML);
@@ -53,7 +64,7 @@ document.body.insertAdjacentHTML('afterbegin', toolbarHTML);
 
 
 //Toolbar
-const toolbar = document.getElementById('toolbar');
+const toolbar = document.getElementById('toolbarMain');
 const fontAlterationButton = document.getElementById('fontAlterationButton');
 const fontAlterationMenu = document.getElementById('fontAlterationMenu');
 let isDragging = false;
@@ -97,74 +108,133 @@ document.addEventListener('mouseup', () => {
     toolbar.style.cursor = 'grab';
 });
 //End of Toolbar
-
+ 
 //Font Alteration
+const MainButtons = document.getElementsByClassName('mainButtons');
+const fontAlterationButtons = document.getElementsByClassName('fontAlterationButtons');
+
 fontAlterationButton.addEventListener('click', () => {
-    // Get the position of the toolbar
-    const toolbarRect = toolbar.getBoundingClientRect();
-    const toolbarLeft = toolbarRect.left;
-    const toolbarTop = toolbarRect.top;
+    for (let button of fontAlterationButtons) {
+        button.classList.toggle('show');
+    }    // Replace the content of toolbar with new buttons
 
-    // Position the menu to the right of the toolbar with an offset of 10px
-    fontAlterationMenu.style.left = (toolbarLeft + toolbar.offsetWidth + 10) + "px";
+    for (let button of MainButtons) {
+        button.classList.toggle('hide');
+    }    // Replace the content of toolbar with new buttons
+   
+    // Add event listeners for the new buttons
+    const backButton = document.getElementById('backButtonFA');
+    const fontAlterationMenuButton = document.getElementById('fontConversionButton');
+    const bionicModeButton = document.getElementById('bionicButton');
+    const toDefaultButton = document.getElementById('toDefaultButton');
 
-    // Position the menu vertically aligned with the toolbar
-    fontAlterationMenu.style.top = toolbarTop + "px";
+    function backButtonClickHandler() {
+        for (let button of fontAlterationButtons) {
+            button.classList.toggle('show');
+        }
+        
+        for (let button of MainButtons) {
+            button.classList.toggle('hide');
+        }
 
-    fontAlterationMenu.classList.toggle('show');
+        backButton.removeEventListener('click', backButtonClickHandler);
+    }
+    backButton.addEventListener('click', backButtonClickHandler);
+
+    fontAlterationMenuButton.addEventListener('click', () => {
+        const toolbarRect = toolbar.getBoundingClientRect();
+        const toolbarLeft = toolbarRect.left;
+        const toolbarTop = toolbarRect.top;
+
+        // Position the menu to the right of the toolbar with an offset of 10px
+        fontAlterationMenu.style.left = (toolbarLeft + toolbar.offsetWidth + 10) + "px";
+
+        // Position the menu vertically aligned with the toolbar
+        fontAlterationMenu.style.top = toolbarTop + "px";
+
+        fontAlterationMenu.classList.toggle('show');
+    });
+
+    bionicModeButton.addEventListener('click', () => {
+        console.log('Bionic Mode Button Clicked');
+    });
+
+    toDefaultButton.addEventListener('click', () => {
+        console.log('To Default Button Clicked');
+    });
 });
 //End of Font Alteration
 
-document.getElementById("taskListButton").addEventListener("click", function() {
 
-});
+//PB -------------------------------------------------------------------------------------------
+const PBButtons = document.getElementsByClassName('PBButtons');
+const PBButton = document.getElementById('PBButton');
+const PBMenu = document.getElementById('PBMenu');
+const PBBar = document.getElementById('PBLiveProgress')
 
-document.getElementById("progressBarButton").addEventListener("click", function() {
+const addPBEventListeners = () => {
+  const backButton = document.getElementById('backButtonPB');
+  const PBDragFlag = document.getElementById('PBDragFlag');
+  const PBDelete = document.getElementById('PBDelete');
 
-});
 
-document.getElementById("timersButton").addEventListener("click", function() {
+PBBar.classList.toggle('show');
+  function backButtonClickHandler() {
+    for (let button of PBButtons) {
+      button.classList.toggle('show');
+    }
 
-});
+    for (let button of MainButtons) {
+      button.classList.toggle('hide');
+    }
 
-document.getElementById("fidgetButton").addEventListener("click", function() {
+    backButton.removeEventListener('click', backButtonClickHandler);
+  }
+  backButton.addEventListener('click', backButtonClickHandler);
 
-});
+  const flag = document.getElementById("flag");
+  let progress = 0;
 
-document.getElementById("AGEButton").addEventListener("click", function() {
+  flag.addEventListener("dragstart", (event) => {
+    event.dataTransfer.setData("flag", event.target.id);
+  });
+  
+  flag.addEventListener("dragend", () => {
+    const flagPosition = flag.getBoundingClientRect();
+    const centerY = window.innerHeight / 2;
+    const distance = Math.abs(centerY - flagPosition.top);
+    const maxDistance = window.innerHeight / 2;
+    progress = (distance / maxDistance) * 100;
+    const progressElement = document.getElementById("PBLiveProgress");
+    progressElement.style.width = `${progress}%`;
+    progressElement.style.display = "block";
+  });
 
-});
+  document.body.addEventListener("dragover", (event) => {
+    event.preventDefault();
+  });
+  
+  document.body.addEventListener("drop", (event) => {
+    event.preventDefault();
+    const flagId = event.dataTransfer.getData("flag");
+    const flagElement = document.getElementById(flagId);
+    const dropZone = event.target;
+    dropZone.appendChild(flagElement);
+  });
 
-document.getElementById("settingsButton").addEventListener("click", function() {
+  PBDelete.addEventListener('click', () => {
+    console.log('Delete Clicked');
+  });
+};
 
-});
+PBButton.addEventListener('click', () => {
+  for (let button of PBButtons) {
+    button.classList.toggle('show');
+  }
 
-const progressBar = document.getElementById("progress-bar");
-const flag = document.getElementById("flag");
+  for (let button of MainButtons) {
+    button.classList.toggle('hide');
+  }
 
-let progress = 0;
-
-flag.addEventListener("dragstart", (event) => {
-  event.dataTransfer.setData("flag", event.target.id);
-});
-
-flag.addEventListener("dragend", () => {
-  const flagPosition = flag.getBoundingClientRect();
-  const centerX = window.innerWidth / 2;
-  const distance = Math.abs(centerX - flagPosition.left);
-  const maxDistance = window.innerWidth / 2;
-  progress = (distance / maxDistance) * 100;
-  progressBar.style.width = `${progress}%`;
-});
-
-document.body.addEventListener("dragover", (event) => {
-  event.preventDefault();
-});
-
-document.body.addEventListener("drop", (event) => {
-  event.preventDefault();
-  const flagId = event.dataTransfer.getData("flag");
-  const flagElement = document.getElementById(flagId);
-  const dropZone = event.target;
-  dropZone.appendChild(flagElement);
+  addPBEventListeners();
 });
